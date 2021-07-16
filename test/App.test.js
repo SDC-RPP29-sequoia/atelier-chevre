cf6b4b9d116469417269b061a068fdb090e57198
Chris McGee <cmac0351@gmail.com>
2021-07-16 07:04:13 -0500

    moved enzyme config to setupTests.js file so it doesn't need to be set up for each test file

--- test/App.test.js
+++ test/App.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import App from '../client/src/components/App.jsx';
-import enzyme, { shallow } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { shallow } from 'enzyme';
 
-enzyme.configure({adapter: new Adapter})
-
-describe('first section', () => {
+describe('App component', () => {
   it('renders without crashing', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toBeTruthy();
