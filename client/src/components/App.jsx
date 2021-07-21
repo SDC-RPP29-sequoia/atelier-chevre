07c992e411736a504e28e2a0f4e031bb3039c543
Chris McGee <cmac0351@gmail.com>
2021-07-21 15:58:18 -0500

    revert changes to App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles.css';
-import Stars from './Stars';
 
 import Header from './Header.jsx';
 
@@ -16,18 +15,6 @@ class App extends React.Component {
     return (
       <div className="wrapper">
         <Header currentProduct={this.state.currentProduct} />
-        <div>
-          <Stars avg="2.75"/>
-        </div>
-        <div>
-          <Stars avg="2.25"/>
-        </div>
-        <div>
-          <Stars avg="4.5"/>
-        </div>
-        <div>
-          <Stars avg="3.25"/>
-        </div>
       </div>
     );
   }
