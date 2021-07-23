983d4dc8deb9e3feaf10bbac271a5499c6e2f4b8
Chris McGee <cmac0351@gmail.com>
2021-07-23 16:08:38 -0500

    improve logic for getting average reveiews

--- client/src/components/Reviews/RatingsAndReviews.jsx
+++ client/src/components/Reviews/RatingsAndReviews.jsx
@@ -6,19 +6,30 @@ import Stars from './Stars';
 class RatingsAndReviews extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentProductReviews: {},
+      currentProductMeta: {}
+    };
   }
 
-  getAverageRating (data) {
-    let totalRating = 0;
-    data.results.forEach(review => {
-      totalRating += review.rating;
-    });
+  componentDidMount () {
+    // get reviews
 
-    return totalRating / data.results.length;
+    // get meta data
+  }
+
+  getAverageRating ({results}) {
+    const averageRating = results.reduce((average, review) => {
+      return average + review.rating;
+    }, 0) / results.length;
+
+    return averageRating;
   }
 
 
   render () {
+    // figure out - only render once data has been returned for a product
 
     const averageRating = this.getAverageRating(ratingsDummyData);
 
@@ -46,32 +57,81 @@ export default RatingsAndReviews;
 
 
 const ratingsDummyData = {
-  'product': '28212',
+  'product': '28213',
   'page': 0,
   'count': 5,
   'results': [
     {
-      'review_id': 407542,
-      'rating': 5,
-      'summary': 'This product was great!',
+      'review_id': 407544,
+      'rating': 4,
+      'summary': 'I am liking these glasses',
       'recommend': true,
-      'response': '',
-      'body': 'I really did or did not like this product based on whether it was sustainably sourced. Then I found out that its made from nothing at all.',
-      'date': '2019-01-01T00:00:00.000Z',
-      'reviewer_name': 'funtime',
-      'helpfulness': 12,
+      'response': 'Glad you\'re enjoying the product!',
+      'body': 'They are very dark. But that\'s good because I\'m in very sunny spots',
+      'date': '2019-06-23T00:00:00.000Z',
+      'reviewer_name': 'bigbrotherbenjamin',
+      'helpfulness': 7,
       'photos': []
     },
     {
-      'review_id': 407543,
+      'review_id': 407546,
+      'rating': 3,
+      'summary': 'I\'m enjoying wearing these shades',
+      'recommend': true,
+      'response': '',
+      'body': 'Comfortable and practical.',
+      'date': '2019-04-14T00:00:00.000Z',
+      'reviewer_name': 'shortandsweeet',
+      'helpfulness': 5,
+      'photos': [
+        {
+          'id': 731061,
+          'url': 'https://images.unsplash.com/photo-1560570803-7474c0f9af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80'
+        },
+        {
+          'id': 731062,
+          'url': 'https://images.unsplash.com/photo-1561693532-9ff59442a7db?ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'
+        },
+        {
+          'id': 731063,
+          'url': 'https://images.unsplash.com/photo-1487349384428-12b47aca925e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+        }
+      ]
+    },
+    {
+      'review_id': 407545,
       'rating': 4,
+      'summary': 'They look good on me',
+      'recommend': true,
+      'response': '',
+      'body': 'I so stylish and just my aesthetic.',
+      'date': '2019-03-12T00:00:00.000Z',
+      'reviewer_name': 'fashionperson',
+      'helpfulness': 1,
+      'photos': []
+    },
+    {
+      'review_id': 407548,
+      'rating': 2,
       'summary': 'This product was ok!',
       'recommend': false,
       'response': '',
-      'body': 'I really did not like this product solely because I am tiny and do not fit into it.',
-      'date': '2019-01-11T00:00:00.000Z',
-      'reviewer_name': 'mymainstreammother',
-      'helpfulness': 2,
+      'body': 'They\'re fine but I wouldn\'t buy again.',
+      'date': '2019-05-23T00:00:00.000Z',
+      'reviewer_name': 'anyone',
+      'helpfulness': 0,
+      'photos': []
+    },
+    {
+      'review_id': 407547,
+      'rating': 5,
+      'summary': 'I\'m not a fan!',
+      'recommend': false,
+      'response': 'Sorry to hear. Is there anything in particular you don\'t like?',
+      'body': 'I don\'t like them',
+      'date': '2019-06-16T00:00:00.000Z',
+      'reviewer_name': 'negativity',
+      'helpfulness': 0,
       'photos': []
     }
   ]
