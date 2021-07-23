f500c5d20d4b378f0289e1ed01cce3b9bab370af
Matthew Saxe <mattsaxe17@gmail.com>
2021-07-22 20:17:13 -0400

    Remove bundle.js for pull

---
+++ client/src/components/ProductOverview.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class ProductOverview extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="product-overview">
+        <div id="announcement-banner">
+        </div>
+
+        <div id="product-main">
+          <div id="product-images"></div>
+          <div id="product-details"></div>
+        </div>
+
+        <div id="product-extra">
+          <div id="product-description"></div>
+          <div id="product-features"></div>
+        </div>
+      </div>
+    );
+  }
+}
