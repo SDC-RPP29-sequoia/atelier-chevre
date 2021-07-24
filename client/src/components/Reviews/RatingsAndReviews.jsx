f4d854c161e2c0254f95892f50f64511e907771a
Chris McGee <cmac0351@gmail.com>
2021-07-23 23:34:33 -0500

    More reviews button working to get 2 reviews at a time

--- client/src/components/Reviews/RatingsAndReviews.jsx
+++ client/src/components/Reviews/RatingsAndReviews.jsx
@@ -12,7 +12,7 @@ class RatingsAndReviews extends React.Component {
     super(props);
 
     this.state = {
-      currentProductReviews: {},
+      currentProductReviews: [],
       currentProductMeta: {},
       displayedReviewsCount: 2
     };
@@ -27,37 +27,44 @@ class RatingsAndReviews extends React.Component {
   }
 
   getReviews () {
-    API.getProductReviews(this.props.productId).then(res => {
+    API.getProductReviews(this.props.productId).then(response => {
       this.setState({
-        currentProductReviews: res
+        currentProductReviews: response.results
       });
     });
   }
 
-  getAverageRating ({results}) {
-    const averageRating = results.reduce((average, review) => {
+  getAverageRating (reviews) {
+    const averageRating = reviews.reduce((average, review) => {
       return average + review.rating;
-    }, 0) / results.length;
+    }, 0) / reviews.length;
 
     return averageRating.toFixed(1);
   }
 
-  getPercentRecommended ({results}) {
-    const percentRecommended = (results.reduce((recommended, review) => {
+  getPercentRecommended (reviews) {
+    const percentRecommended = (reviews.reduce((recommended, review) => {
       if (review.recommend) {
         return recommended + 1;
       } else {
         return recommended;
       }
-    }, 0) / results.length) * 100;
+    }, 0) / reviews.length) * 100;
 
     return percentRecommended.toFixed();
+  }
+
+  handleMoreReviewsClick () {
+    const newCount = this.state.displayedReviewsCount + 2;
+    this.setState({
+      displayedReviewsCount: newCount
+    });
   }
 
 
   render () {
     // figure out - only render once data has been returned for a product
-    if (this.state.currentProductReviews.results === undefined) {
+    if (this.state.currentProductReviews.length === 0) {
       return (
         <div></div>
       );
@@ -65,8 +72,8 @@ class RatingsAndReviews extends React.Component {
 
     const averageRating = this.getAverageRating(this.state.currentProductReviews);
     const percentRecommended = this.getPercentRecommended(this.state.currentProductReviews);
-    const totalReviews = this.state.currentProductReviews.results.length;
-    const displayedReviews = this.state.currentProductReviews.results.slice(0, this.state.displayedReviewsCount);
+    const totalReviews = this.state.currentProductReviews.length;
+    const displayedReviews = this.state.currentProductReviews.slice(0, this.state.displayedReviewsCount);
     const reviewsToDisplay = displayedReviews.map(review => {
       return <ReviewContent key={review.review_id} review={review} />;
     });
@@ -86,7 +93,9 @@ class RatingsAndReviews extends React.Component {
           <div id="reviews-col2">
             <Sort totalReviews={totalReviews}/>
             {reviewsToDisplay}
-            <button className="btn">MORE REVIEWS</button>
+            {this.state.displayedReviewsCount < this.state.currentProductReviews.length &&
+              <button className="btn" onClick={() => this.handleMoreReviewsClick()}>MORE REVIEWS</button>
+            }
             <button className="btn">ADD A REVIEW <span className="plus-icon">+</span></button>
           </div>
         </div>
