a64f07e958a87793fb18a0eff7f3b378c2a1a637
Matthew Saxe <mattsaxe17@gmail.com>
2021-08-03 22:32:50 -0400

    Fix breaking test

--- client/src/components/ProductOverview/ProductExtra.jsx
+++ client/src/components/ProductOverview/ProductExtra.jsx
@@ -4,12 +4,12 @@ import { AiOutlineCheck } from 'react-icons/ai';
 const ProductExtra = (props) => (
   <div id="product-extra">
     <div id="product-description">
-      <h4 id="description-title">{props.product.slogan}</h4>
-      <p id="description">{props.product.description}</p>
+      <h4 id="description-title">{props.product?.slogan}</h4>
+      <p id="description">{props.product?.description}</p>
     </div>
     <div id="product-features">
 
-      {props.product.features?.map(feature => (
+      {props.product?.features?.map(feature => (
         <div className="feature">
           <div className="check">
             <AiOutlineCheck />
