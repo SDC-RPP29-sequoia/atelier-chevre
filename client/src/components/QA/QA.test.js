448fdadb9c2e034a9acf08c7c8bc6c4dee0da61a
Sunik Kim <sunik.h.kim@gmail.com>
2021-08-04 13:49:53 -0400

    refactoring tests

--- client/src/components/QA/QA.test.js
+++ client/src/components/QA/QA.test.js
@@ -13,128 +13,66 @@ import LoadMoreAnswers from './LoadMoreAnswers';
 import QHelpfulAddAnswer from './QHelpfulAddAnswer';
 import SignatureHelpfulReport from './SignatureHelpfulReport';
 
-describe('QuestionsAndAnswers component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<QuestionsAndAnswers />);
-    expect(wrapper).toBeTruthy();
+describe('Questions & Answers component', () => {
+  let QA, header, searchBar, list, buttons, answerForm, questionForm, answers, loadMoreAs, QHelpful, signatureHelpful;
+
+  beforeEach(() => {
+    QA = shallow(<QuestionsAndAnswers />);
+    header = shallow(<QAHeader />);
+    searchBar = shallow(<SearchBar />);
+    list = shallow(<QAList filteredQs={QA.state.filteredQs} answers={QA.state.answers} />);
+    buttons = shallow(<QAButtons />);
+    answerForm = shallow(<AnswerModal photos={QA.state.photos}/>);
+    questionForm = shallow(<QuestionModal />);
+    answers = shallow(<AnswerContainer photos={QA.state.photos}/>);
+    loadMoreAs = shallow(<LoadMoreAnswers />);
+    QHelpful = shallow(<QHelpfulAddAnswer />);
+    signatureHelpful = shallow(<SignatureHelpfulReport />);
+  });
+
+  it('wrapper renders without crashing', () => {
+    expect(QA).toBeTruthy();
+  });
+
+  it('header renders without crashing', () => {
+    expect(header).toBeTruthy();
+  });
+
+  it('search bar renders without crashing', () => {
+    expect(searchBar).toBeTruthy();
+  });
+
+  it('list renders without crashing', () => {
+    expect(list).toBeTruthy();
+  });
+
+  it('buttons renders without crashing', () => {
+    expect(buttons).toBeTruthy();
+  });
+
+  it('answer form renders without crashing', () => {
+    expect(answerForm).toBeTruthy();
+  });
+
+  it('question form renders without crashing', () => {
+    expect(questionForm).toBeTruthy();
+  });
+
+  it('answers renders without crashing', () => {
+    expect(answers).toBeTruthy();
+  });
+
+  it('load more answers renders without crashing', () => {
+    expect(loadMoreAs).toBeTruthy();
+  });
+
+  it('q helpful add answer renders without crashing', () => {
+    expect(QHelpful).toBeTruthy();
+  });
+
+  it('signature helpful report renders without crashing', () => {
+    expect(signatureHelpful).toBeTruthy();
   });
 });
 
-describe('QAHeader component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<QAHeader />);
-    expect(wrapper).toBeTruthy();
-  });
-});
-
-describe('SearchBar component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<SearchBar />);
-    expect(wrapper).toBeTruthy();
-  });
-});
-
-describe('QAList component', () => {
-  let filteredQs = [
-    {
-      'question_id': 213346,
-      'question_body': 'Where is this product made?',
-      'question_date': '2018-10-04T00:00:00.000Z',
-      'asker_name': 'jbilas',
-      'question_helpfulness': 21,
-      'reported': false,
-      'answers': {
-        '1992368': {
-          'id': 1992368,
-          'body': 'China',
-          'date': '2018-08-04T00:00:00.000Z',
-          'answerer_name': 'Seller',
-          'helpfulness': 16,
-          'photos': []
-        }
-      }
-    }
-  ];
-
-  let answers = {
-    '213346': {
-      'data': [
-        {
-          'id': 1992383,
-          'body': 'Michigan',
-          'date': '2018-01-24T00:00:00.000Z',
-          'answerer_name': 'iluvbirds',
-          'helpfulness': 4,
-          'photos': []
-        },
-        {
-          'id': 1992387,
-          'body': 'Made locally!',
-          'date': '2018-11-24T00:00:00.000Z',
-          'answerer_name': 'Seller',
-          'helpfulness': 8,
-          'photos': []
-        }
-      ],
-      'count': 2
-    }
-  };
-
-  it('renders without crashing', () => {
-    const wrapper = shallow(<QAList filteredQs={filteredQs} answers={answers} />);
-    expect(wrapper).toBeTruthy();
-  });
-});
-
-describe('QAButtons component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<QAButtons />);
-    expect(wrapper).toBeTruthy();
-  });
-});
-
-describe('AnswerModal component', () => {
-  let photos = ['photo1', 'photo2', 'photo3'];
-
-  it('renders without crashing', () => {
-    const wrapper = shallow(<AnswerModal photos={photos}/>);
-    expect(wrapper).toBeTruthy();
-  });
-});
-
-describe('QuestionModal component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<QuestionModal />);
-    expect(wrapper).toBeTruthy();
-  });
-});
-
-describe('AnswerContainer component', () => {
-  let photos = ['photo1', 'photo2', 'photo3'];
-
-  it('renders without crashing', () => {
-    const wrapper = shallow(<AnswerContainer photos={photos}/>);
-    expect(wrapper).toBeTruthy();
-  });
-});
-
-describe('LoadMoreAnswers component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<LoadMoreAnswers />);
-    expect(wrapper).toBeTruthy();
-  });
-});
-
-describe('QHelpfulAddAnswer component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<QHelpfulAddAnswer />);
-    expect(wrapper).toBeTruthy();
-  });
-});
-
-describe('SignatureHelpfulReport component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<SignatureHelpfulReport />);
-    expect(wrapper).toBeTruthy();
-  });
-});
+// let photos = ['photo1', 'photo2', 'photo3'];
