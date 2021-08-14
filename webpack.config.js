9eb7f39fde20315a3f89f9af84dee7e40940bd51
Chris McGee <cmac0351@gmail.com>
2021-08-14 00:49:14 -0500

    added option to clean public folder on each build

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'client/public'),
     filename: 'bundle.js',
+    clean: true
   },
   plugins: [new CompressionPlugin({
     filename: '[path][base].br',
